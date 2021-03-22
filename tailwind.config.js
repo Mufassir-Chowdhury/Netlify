2f29a4ee0b5409ff9e4958545338b76251ef91b6
Mufassir-Chowdhury <mac22214u@gmail.com>
2021-03-22 13:16:57 +0600

    packages

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      'sans': 'Helvetica, Arial, sans-serif',
+      'body': 'Helvetica, Arial, sans-serif',
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
